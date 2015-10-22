c8c21ee8ad1b4dca0d2cf9cd442a8da05d5028c8
tslnc04 <timothy.laskoski@gmail.com>
2015-10-21 23:58:17 -0400

    save

--- www/js/detectGenerator.js
+++ www/js/detectGenerator.js
@@ -4,7 +4,7 @@ function generateDetectCommand() {
   var detectAmt = document.getElementById("detectAmt").value;
   var entityData = document.getElementById("entityData").value;
   var blockInformation = document.getElementById("blockInformation").value.split("\n");
-  var blocks
+  var blocks = []
 
   for (var i = 0; i < blockInformation.length; i++) {
     console.log("TEST");
