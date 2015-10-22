d2807f4f0f3a1a06e0d972c85475f60d04390464
tslnc04 <timothy.laskoski@gmail.com>
2015-10-21 23:30:34 -0400

    save

---
+++ js/detectGenerator.js
@@ -0,0 +1,8 @@
+(function() {
+  var entitySelector = document.getElementById("entitySelector").value;
+  var detectObjective = document.getElementById("detectObjective").value;
+  var detectAmt = document.getElementById("detectAmt").value;
+  var entityData = document.getElementById("entityData").value;
+
+  alert(entitySelector);
+})();
