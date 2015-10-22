d12e26e8f31801c45e986a8257af198bcfc3af64
tslnc04 <timothy.laskoski@gmail.com>
2015-10-22 00:17:45 -0400

    save

--- js/detectGenerator.js
+++ js/detectGenerator.js
@@ -16,6 +16,6 @@ function generateDetectCommand() {
   }
 
   for (var j = 0; j < blocks.length; j++) {
-    mainComm += "execute " + entitySelector + " ~ ~ ~ detect " + blocks[j].loc + " " + blocks[j].bname + " " + blocks[j].dv
+    mainComm += "execute " + entitySelector + " ~ ~ ~ detect " + blocks[j].loc + " " + blocks[j].bname + " " + blocks[j].dv + " "
   }
 }
