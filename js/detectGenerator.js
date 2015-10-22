969675240031209c37128afb0ce6e1716b7528b9
tslnc04 <timothy.laskoski@gmail.com>
2015-10-21 23:41:33 -0400

    save

--- js/detectGenerator.js
+++ js/detectGenerator.js
@@ -5,7 +5,7 @@ function generateDetectCommand() {
   var entityData = document.getElementById("entityData").value;
   var blockInformation = document.getElementById("blockInformation").value.split("\n");
 
-  for (var i == 0; i < blockInformation.length; i++) {
+  for (var i = 0; i < blockInformation.length; i++) {
     console.log("TEST");
   }
 }
