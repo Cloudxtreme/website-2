d024ef54797c1761b48d4ecdd59f44b16d55fb2e
tslnc04 <timothy.laskoski@gmail.com>
2015-10-27 06:50:39 -0400

    save

--- www/js/detectGenerator.js
+++ www/js/detectGenerator.js
@@ -45,6 +45,7 @@ function importGeneration() {
   var newSaveCode = prompt("Paste your save code here:","");
   if (newSaveCode) {
     var dataCode = JSON.parse(newSaveCode);
+    console.log(dataCode);
     document.getElementById("entitySelector").value = dataCode.settings.entitySelector;
     this.scoreEntitySelector = dataCode.settings.scoreEntitySelector;
     this.detectObjective = dataCode.settings.detectObjective;
